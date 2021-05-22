627720039524c918e9e65a1ca8757e939bab9a6a
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-22 21:56:52 +0200

    Added MQTT sender

--- src/io.rs
+++ src/io.rs
@@ -44,7 +44,6 @@ impl Node for ConsoleOut {
 }
 
 pub struct MqttIn {
-    client: Client,
     connection: Connection,
     outputs: Vec<mpsc::Sender<Arc<Vec<u8>>>>,
 }
@@ -57,7 +56,6 @@ impl MqttIn {
         let (mut client, connection) = Client::new(mqttoptions, 10);
         client.subscribe(topic, QoS::ExactlyOnce).unwrap();
         Self {
-            client,
             connection,
             outputs: vec![],
         }
@@ -88,6 +86,53 @@ impl Node for MqttIn {
                     },
                     _ => (),
                 },
+                _ => (),
+            }
+        }
+    }
+}
+
+pub struct MqttOut {
+    client: Client,
+    outputs: Vec<mpsc::Sender<Arc<Vec<u8>>>>,
+    chan: (mpsc::Sender<Arc<Vec<u8>>>, mpsc::Receiver<Arc<Vec<u8>>>),
+    topic: &'static str,
+}
+
+impl MqttOut {
+    pub fn new(server: &str, port: u16, topic: &'static str) -> Self {
+        let mut mqttoptions = MqttOptions::new(&format!("rust_flowmsg_"), server, port);
+        mqttoptions.set_keep_alive(5);
+
+        let (mut client, connection) = Client::new(mqttoptions, 10);
+        Self {
+            client,
+            outputs: vec![],
+            chan: mpsc::channel(),
+            topic,
+        }
+    }
+}
+
+impl Node for MqttOut {
+    fn get_sender(&self) -> mpsc::Sender<Arc<Vec<u8>>> {
+        self.chan.0.clone()
+    }
+
+    fn add_output(&mut self, _: mpsc::Sender<Arc<Vec<u8>>>) {
+        panic!("Attempting to subscribe to a recieve-only node")
+    }
+
+    fn run(&mut self) {
+        loop {
+            match self.chan.1.recv() {
+                Ok(val) => {
+                    if self
+                        .client
+                        .publish(self.topic, QoS::ExactlyOnce, false, val.to_vec())
+                        .is_err()
+                    {}
+                }
                 _ => (),
             }
         }
