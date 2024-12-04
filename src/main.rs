d78c5766504888b322a73f30e833c1a2b52a3a44
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2024-12-04 11:26:10 +0100

    Optimisations

--- src/main.rs
+++ src/main.rs
@@ -11,39 +11,35 @@ fn main() {
     let raw_input = std::fs::read_to_string(std::env::args().nth(1).expect("Provide conf file"))
         .expect("Failed to read conf file")
         .split("\n")
-        .map(|x| x.split("\t").map(|x| x.to_owned()).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    for input in raw_input {
-        match input[0].as_str() {
+        .map(|x| x.split("\t"));
+    for mut input in raw_input {
+        let node_name = input.next().expect("No node name provided");
+        let node_id = input.next().expect("No node id provided");
+        match node_name {
             "mqtt_in" => {
-                let id = input[1].clone();
-                let node = MqttIn::new(&input[2], input[3].parse().unwrap(), &input[4]);
-                nodes.insert(id, Box::new(node));
+                let node = MqttIn::new(input.next().unwrap(), input.next().unwrap().parse().unwrap(), input.next().unwrap());
+                nodes.insert(node_id.to_string(), Box::new(node));
             }
             "mqtt_out" => {
-                let id = input[1].clone();
-                let node = MqttOut::new(&input[2], input[3].parse().unwrap(), input[4].clone());
-                nodes.insert(id, Box::new(node));
+                let node = MqttOut::new(input.next().unwrap(), input.next().unwrap().parse().unwrap(), input.next().unwrap().to_string());
+                nodes.insert(node_id.to_string(), Box::new(node));
             }
             "rbe" => {
-                let id = input[1].clone();
-                nodes.insert(id.clone(), Box::new(ReportByException::new(&id)));
+                nodes.insert(node_id.to_string(), Box::new(ReportByException::new(node_id)));
             }
             "static_out" => {
-                let id = input[1].clone();
-                nodes.insert(id, Box::new(StaticOutput::new(input[2].clone())));
+                nodes.insert(node_id.to_string(), Box::new(StaticOutput::new(input.next().unwrap().to_string())));
             }
             "dbg" => {
-                let id = input[1].clone();
-                nodes.insert(id, Box::new(ConsoleOut::new(input[2].clone())));
+                nodes.insert(node_id.to_string(), Box::new(ConsoleOut::new(input.next().unwrap().to_string())));
             }
             "link" => {
                 let right = nodes
-                    .get(&input[2].clone())
+                    .get(input.next().unwrap())
                     .expect("Right node not found")
                     .get_sender();
                 let left = nodes
-                    .get_mut(&input[1].clone())
+                    .get_mut(node_id)
                     .expect("Left node not found");
                 left.add_output(right);
             }
